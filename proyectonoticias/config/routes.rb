4dbfd2597189a4516e2c0e3f21dcc9266274d37c
Alberto Cols <betocols@betocols.betocols>
2014-04-05 12:50:22 -0400

    home#index, migrate reports arreglado

--- proyectonoticias/config/routes.rb
+++ proyectonoticias/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :users
 
   resources :reports
@@ -7,8 +9,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'home#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
