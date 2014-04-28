7413cbdce61cb84f109b10f79420a6c4a02210c6
Alberto Cols <betocols@betocols.betocols>
2014-04-28 17:34:30 -0400

    cosas

--- proyectonoticias/app/controllers/users_controller.rb
+++ proyectonoticias/app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'Usuario creado exitosamente.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Usuario actualizado exitosamente.' }
         format.json { render action: 'show', status: :ok, location: @user }
       else
         format.html { render action: 'edit' }
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url }
+      format.html { redirect_to users_url, notice: 'Usuario eliminado exitosamente.' }
       format.json { head :no_content }
     end
   end
