577383b55e42c1e00c18b59ac14d500f64d8e692
Alberto Cols <betocols@betocols.betocols>
2014-04-29 00:59:14 -0400

    schema

--- proyectonoticias/db/schema.rb
+++ proyectonoticias/db/schema.rb
@@ -31,23 +31,20 @@ ActiveRecord::Schema.define(version: 20140427113401) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "login",               default: "", null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "login"
     t.string   "name"
     t.string   "lastname"
     t.integer  "gid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["login"], name: "index_users_on_login", unique: true
 
 end
